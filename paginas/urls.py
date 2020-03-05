c5a8df120ae9e509718a1e9931c61be24c15e9fd
Julio Delgado <juliom6@gmail.com>
2020-03-05 11:23:18 -0300

    adicionado o unixtimestamp

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PaginaInicioView
+from .views import PaginaInicioView, tempoView
 
 
 urlpatterns = [
     path('', PaginaInicioView.as_view(), name='inicio'),
+    path('tempo', tempoView, name='tempo'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
-
+from django.http import JsonResponse
+from datetime import datetime
+from pytz import timezone
+import time
 
 class PaginaInicioView(TemplateView):
     template_name = 'inicio.html'
+
+def tempoView(request):
+    if request.method == 'GET':
+        return JsonResponse({'unixtime': int(time.mktime(datetime.now(timezone('America/Sao_Paulo')).timetuple()))})
