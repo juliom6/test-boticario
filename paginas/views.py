9817e6799fcfdaa355eaab6ca2da7ad80355054d
Julio Delgado <juliom6@gmail.com>
2020-03-25 12:54:19 -0300

    adicionado os metodos get e post re recepcao

--- paginas/views.py
+++ paginas/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.http import JsonResponse
+from django.http import HttpResponse
 from datetime import datetime
 import time
 
@@ -12,4 +13,19 @@ def tempoView(request):
 
 def contatoView(request):
     if request.method == 'GET':
-        return JsonResponse({'a' : 'b'}, status=200)
+        print(request.GET.get('nome'))
+        print(request.GET.get('email'))
+        print(request.GET.get('mensagem'))
+        nome = request.GET.get('nome')
+        email = request.GET.get('email')
+        mensagem = request.GET.get('mensagem')
+        return HttpResponse('<h2>Obrigado ' + nome + '!!</h2>', content_type='text/html', status=201)
+
+    if request.method == 'POST':
+        print(request.POST.get('nome'))
+        print(request.POST.get('email'))
+        print(request.POST.get('mensagem'))
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        mensagem = request.POST.get('mensagem')
+        return HttpResponse('<h2>Obrigado ' + nome + '!!</h2>', content_type='text/html', status=201)
